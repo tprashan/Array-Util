09d06ae7f9a7f2c6772d1e4ad8063753aa16111b
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-01-21 17:27:03 +0530

    resize , count,findFirst, findLast,filter is almost done.

--- arrayUtil.c
+++ arrayUtil.c
@@ -7,7 +7,7 @@ int areEqual(ArrayUtil array1, ArrayUtil array2){
 	int i,length;
 	char *arr1= (char *)(array1.base);
 	char *arr2= (char *)array2.base;
-	length = array1.length<=array2.length?(array2.length*array1.typeSize):(array1.length*array1.typeSize);
+	length = array1.length*array1.typeSize;
 
 	if(array1.length!=array2.length || array1.typeSize!=array2.typeSize)
 		return 0;
@@ -19,25 +19,25 @@ int areEqual(ArrayUtil array1, ArrayUtil array2){
 };
 
 ArrayUtil create(int typeSize, int length){
-	int *array = calloc(length,sizeof(typeSize));
 	ArrayUtil array1;
-	array1.base = array;
+	array1.base = calloc(length,typeSize);;
 	array1.typeSize = typeSize;
 	array1.length = length;
 	return array1;
 };
 
 ArrayUtil resize(ArrayUtil util, int length){
-	int i;
-	int *array= (int *)(util.base);
-	int *util_1 = calloc(length,sizeof(util.typeSize));
+	int i,newIndex;
+	int resizeLength = length*util.typeSize;
+	int byteLength =util.length*util.typeSize;	
+	int resizeByte = (length-util.length)*util.typeSize;
 
-	for(i=0;i<length;i++){
-		if(i<=util.length)
-			util_1[i]=array[i];
-	};
+	util.base = realloc(util.base,resizeLength);
 	util.length = length;
-	util.base = util_1;
+	for(i = 0;i < resizeByte;i++){
+		newIndex = byteLength + i;
+		((char*)util.base)[newIndex] = 0;
+	}
 	return util;
 };
 
@@ -67,18 +67,90 @@ void dispose(ArrayUtil util){
 
 void* findFirst(ArrayUtil util, MatchFunc* match, void* hint){
 	int i,count=0,j=0,result;
-	char *array= (char *)(util.base);
 	int memoryLength =util.length*util.typeSize;
-	for (i=0;i<memoryLength;i++){
-		if(count!=(util.typeSize)*j)
-			count++;
-		if(count==(util.typeSize)*j){
-			j++;
-			result = match(hint,&array[count]);
-			if(result==1){
-				return &array[i];
-			}
-		};
+	for (i=0;i<memoryLength;){
+		result = match(hint,&(util.base[i]));
+		if(result==1){
+			return &(util.base[i]);
+		}
+		i = i + util.typeSize;
 	};
 	return NULL;
 };
+
+void* findLast(ArrayUtil util, MatchFunc* match, void* hint){
+	int i,count=0,j=0,result;
+	int memoryLength =util.length*util.typeSize;
+	int lastPosition = memoryLength-util.typeSize;
+	for (i=lastPosition;i>=0;){
+		result = match(hint,&(util.base[i]));
+		if(result==1)
+			return &(util.base[i]);
+		i=i-util.typeSize;
+	};
+	return NULL;
+};
+
+int count(ArrayUtil util, MatchFunc* match, void* hint){
+	int i,count=0,result;
+	int memoryLength =util.length*util.typeSize;
+	for (i=0;i<memoryLength;){
+		result = match(hint,&(util.base[i]));
+		if(result==1)
+			count++;
+		i = i + util.typeSize;
+	};
+	return count;
+};
+  // memcpy((destination),&(util.base[i]),j+util.typeSize);
+  // *destination = realloc(*destination,util.typeSize*j);
+// int filter(ArrayUtil util, MatchFunc* match, void* hint, void** destination, int maxItems ){
+// 	int i,count=0,j=0;
+// 	int bytelength= util.typeSize*util.length;
+// 	*destination  = malloc(bytelength);
+//     printf("%d\n",util.typeSize );
+//     for(i=0;i<bytelength;){
+//       if(match(hint,&(util.base[i]))){
+//         *(char *)(destination)[j]=*(char *)&(util.base[i]);
+//         printf("%d\n",*(char *)(destination)[j] );
+//         count++;
+//         j=j+util.typeSize;
+//         printf("%d\n",*(char *)(destination)[j] );
+//       };
+//       if(count==maxItems)
+//       	break;
+//       i = i+util.typeSize;
+//    }
+//    return count;
+// };
+
+int filter(ArrayUtil util, MatchFunc* match, void* hint, void** destination, int maxItems ){
+	int i,count=0;
+	int bytelength= util.typeSize*util.length;
+   	*destination  = malloc(util.typeSize);
+   	for(i=0;i<bytelength;i++){
+    	if(match(hint,&util.base[i])){
+	  		memcpy(&((*destination)[count*util.typeSize]),&(util.base[i]),util.typeSize);
+	        count++;
+  			*destination = realloc(*destination,util.typeSize*count+1);
+      	};
+      	if(count==maxItems)
+      		break;
+   	}
+   return count;
+};
+
+// int filter(ArrayUtil array, MatchFunc* match, void* hint, void** destination, int maxItems ){
+// 	int i,length=0;
+// 	*destination = malloc(array.typeSize);
+// 	for(i=0;i<array.length*array.typeSize;i++){
+// 		if(match(hint,&array.base[i])){
+// 			memcpy(&((*destination)[length*array.typeSize]),&(array.base[i]),array.typeSize);
+// 			length++;
+// 			*destination = realloc(*destination,array.typeSize*length+1);
+// 		}  
+// 		if(maxItems==(length))
+// 			return maxItems;
+// 	};
+// 	return length;
+// };
--- arrayUtilTest.c
+++ arrayUtilTest.c
@@ -8,16 +8,8 @@ void test_array_util_equal(){
 	int a[]={1,2,3,4,5};
 	int b[]={1,2,3,4,5,6};
 
-	ArrayUtil array1;
-	ArrayUtil array2;
-
-	array1.base = a;
-	array1.typeSize = sizeof(int);
-	array1.length = 5;
-
-	array2.base = b;
-	array2.typeSize = sizeof(int);
-	array2.length = 6;
+	ArrayUtil array1 = { a,sizeof(int),5};
+	ArrayUtil array2 = { b,sizeof(int),6};
 
 	assertEqual(areEqual(array1, array2),0);
 
@@ -27,16 +19,8 @@ void test_ArrayUtil_same_array_return_1(){
 	int a[]={1,2,3,4,5};
 	int b[]={1,2,3,4,5};
 
-	ArrayUtil array1;
-	ArrayUtil array2;
-
-	array1.base = a;
-	array1.typeSize = sizeof(int);
-	array1.length = 5;
-
-	array2.base = b;
-	array2.typeSize = sizeof(int);
-	array2.length = 5;
+	ArrayUtil array1 = { a,sizeof(int),5};
+	ArrayUtil array2 = { b,sizeof(int),5};
 
 	assertEqual(areEqual(array1, array2),1);
 
@@ -46,16 +30,8 @@ void test_ArrayUtil_same_float_array_return_1(){
 	float a[]={1,2.1,3,4,5};
 	float b[]={1,2.1,3,4,5};
 
-	ArrayUtil array1;
-	ArrayUtil array2;
-
-	array1.base = a;
-	array1.typeSize = sizeof(float);
-	array1.length = 5;
-
-	array2.base = b;
-	array2.typeSize = sizeof(float);
-	array2.length = 5;
+	ArrayUtil array1 = { a,sizeof(float),5};
+	ArrayUtil array2 = { b,sizeof(float),5};
 
 	assertEqual(areEqual(array1, array2),1);
 
@@ -66,25 +42,16 @@ void test_ArrayUtil_diff_char_array_return_0(){
 	float a[]={1,2.1,3,4,5};
 	char b[]={'c','b','r','s','x','\0'};
 
-	ArrayUtil array1;
-	ArrayUtil array2;
-
-	array1.base = a;
-	array1.typeSize = sizeof(float);
-	array1.length = 5;
-
-	array2.base = b;
-	array2.typeSize = sizeof(char);
-	array2.length = 5;
+	ArrayUtil array1 = { a,sizeof(float),5};
+	ArrayUtil array2 = { b,sizeof(char),5};
 
 	assertEqual(areEqual(array1, array2),0);
 
 };
 
 void test_ArrayUtil_create(){
-	ArrayUtil array;
 	int *a;
-	array = create(4,3);
+	ArrayUtil array = create(4,3);
 	a = array.base;
 	assertEqual(a[0],0);
 	assertEqual(sizeof(a),4);
@@ -103,65 +70,95 @@ void test_ArrayUtil_create_array_typeSize_is_4(){
 };
 
 void test_ArrayUtil_resize_array(){
-	int a[]={1,2,3,4,5};
-	int *resArray;
-	ArrayUtil array;
-	ArrayUtil resizeArray;
-	array.base = a;
-	array.typeSize = sizeof(int);
-	array.length = 5;
-	resizeArray=resize(array,7); 
-	resArray = resizeArray.base;
-	assertEqual(resizeArray.typeSize,4);
-	assertEqual(resizeArray.length,7);
+	ArrayUtil result;
+	ArrayUtil array = create(4,4);
+	((int *)array.base)[0] = 1;
+	((int *)array.base)[1] = 2;
+	((int *)array.base)[2] = 4;
+	((int *)array.base)[3] = 5;
+	result = resize(array,3);
+	assertEqual(result.typeSize,4);
+	assertEqual(result.length,3);
 };
+
+void test_ArrayUtil_resize_array_shoud_give_4_at_index_3(){
+	ArrayUtil result;
+	ArrayUtil array = create(4,4);
+	((int *)array.base)[0] = 1;
+	((int *)array.base)[1] = 2;
+	((int *)array.base)[2] = 5;
+	((int *)array.base)[3] = 4;
+	result = resize(array,6);
+	assertEqual(result.typeSize,4);
+	assertEqual(((int *)result.base)[3],4);
+};
+
+void test_ArrayUtil_resize_array_shoud_give_8_at_index_2(){
+	ArrayUtil result;
+	ArrayUtil array = create(4,4);
+	((int *)array.base)[0] = 1;
+	((int *)array.base)[1] = 2;
+	((int *)array.base)[2] = 8;
+	((int *)array.base)[3] = 4;
+	result = resize(array,6);
+	assertEqual(result.typeSize,4);
+	assertEqual(((int *)result.base)[2],8);
+};
+
 
 void test_ArrayUtil_resize_array_shoud_give_0_at_index_6(){
-	int a[]={1,2,3,4,5};
-	int *resArray;
-	ArrayUtil array;
-	ArrayUtil resizeArray;
-	array.base = a;
-	array.typeSize = sizeof(int);
-	array.length = 5;
-	resizeArray=resize(array,7); 
-	resArray = resizeArray.base;
-	assertEqual(resArray[6],0);
+	ArrayUtil result;
+	ArrayUtil array = create(4,4);
+	((int *)array.base)[0] = 1;
+	((int *)array.base)[1] = 2;
+	((int *)array.base)[2] = 5;
+	((int *)array.base)[3] = 4;
+	result = resize(array,7);
+	assertEqual(result.typeSize,4);
+	assertEqual(((int *)result.base)[6],0);
 };
 
-void test_ArrayUtil_resize_array_shoud_give_0_at_index_5(){
-	int a[]={1,2,3,4,5};
-	int *resArray;
-	ArrayUtil array;
-	ArrayUtil resizeArray;
-	array.base = a;
-	array.typeSize = sizeof(int);
-	array.length = 5;
-	resizeArray=resize(array,4); 
-	resArray = resizeArray.base;
-	assertEqual(resArray[3],4);
+
+void test_ArrayUtil_resize_array_shoud_give_c_at_index_3(){
+	ArrayUtil result;
+	ArrayUtil array = create(4,4);
+	((char *)array.base)[0] = 'a';
+	((char *)array.base)[1] = 'b';
+	((char *)array.base)[2] = 'g';
+	((char *)array.base)[3] = 'c';
+	result = resize(array,6);
+	assertEqual(result.typeSize,4);
+	assertEqual(((char*)result.base)[3] ,'c');
+};
+
+void test_ArrayUtil_resize_array_shoud_give_df_at_index_1(){
+	ArrayUtil result;
+	ArrayUtil array = create(4,4);
+	((char **)array.base)[0] = "hiiii";
+	((char **)array.base)[1] = "guys";
+	((char **)array.base)[2] = "hiiii";
+	((char **)array.base)[3] = "guys";
+	result = resize(array,5);
+	assertEqual(result.length, 5);
+	assertEqual(((char **)result.base)[2],"hiiii");
+	assertEqual(((int *)result.base)[4],0);
 };
 
 void test_ArrayUtil_indexOf_int(){
 	int a[]={1,2,3,4,5};
 	int Index,ele =3;
 
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(int);
-	array.length = 5;
+	ArrayUtil array = { a,sizeof(int),5};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,2); 
 };
 
+
 void test_ArrayUtil_indexOf_int_return_minus_1(){
 	int a[]={1,2,3,4,5};
 	int Index,ele =8;
-
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(int);
-	array.length = 5;
+	
+	ArrayUtil array = { a,sizeof(int),5};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,-1); 
 };
@@ -171,10 +168,7 @@ void test_ArrayUtil_indexOf_char(){
 	int  Index;
 	char ele ='r';
 
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(char);
-	array.length = 7;
+	ArrayUtil array = { a,sizeof(char),7};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,5); 
 };
@@ -184,10 +178,7 @@ void test_ArrayUtil_indexOf_char_return_minus_1(){
 	int  Index;
 	char ele ='b';
 
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(char);
-	array.length = 7;
+	ArrayUtil array = { a,sizeof(char),7};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,-1); 
 };
@@ -197,10 +188,7 @@ void test_ArrayUtil_indexOf_float(){
 	int Index;
 	float ele =3.1;
 
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(float);
-	array.length = 5;
+	ArrayUtil array = { a,sizeof(float),5};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,2); 
 };
@@ -210,10 +198,7 @@ void test_ArrayUtil_indexOf_float_return_minus_1(){
 	int Index;
 	float ele =3.2;
 
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(float);
-	array.length = 5;
+	ArrayUtil array = { a,sizeof(float),5};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,-1); 
 };
@@ -223,10 +208,7 @@ void test_ArrayUtil_indexOf_float_return_0(){
 	int Index;
 	float ele =1.7;
 
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(float);
-	array.length = 5;
+	ArrayUtil array = { a,sizeof(float),5};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,0); 
 };
@@ -235,10 +217,8 @@ void test_ArrayUtil_indexOf__return_minus_1(){
 	float a[]={1.7,2.8,3.1,4,5.3};
 	int Index;	
 	float ele =2.1;
-	ArrayUtil array;
-	array.base = a;
-	array.typeSize = sizeof(float);
-	array.length = 5;
+	
+	ArrayUtil array = { a,sizeof(float),5};
 	Index = findIndex(array,&ele);
 	assertEqual(Index,-1); 
 };
@@ -257,33 +237,151 @@ int isEven(void* hint, void* item){
 };
 
 void test_for_findFirst(){
-	int a[]={1,5,3,7,4},hint=3,*result;
+	int a[]={1,5,3,4,9},hint=3,*result;
 
-	ArrayUtil array1;
-	array1.base = a;
-	array1.typeSize = sizeof(int);
-	array1.length = 5;
-	result = findFirst(array1,isEven,&hint);
+	ArrayUtil array = { a,sizeof(int),5};
+	result = findFirst(array,isEven,&hint);
 	assertEqual(*result,4);
 };
 
 void test_for_findFirst_return_NULL(){
 	int a[]={1,5,3,7,9},hint=3,*result;
 
-	ArrayUtil array1;
-	array1.base = a;
-	array1.typeSize = sizeof(int);
-	array1.length = 5;
-	assertEqual(findFirst(array1,isEven,&hint),(int)NULL);
-}
+	ArrayUtil array = { a,sizeof(int),5};
+	assertEqual(findFirst(array,isEven,&hint),(int)NULL);
+};
 
 void test_for_findFirst_return_6(){
 	int a[]={1,5,6,4,9},hint=3,*result;
 
-	ArrayUtil array1;
-	array1.base = a;
-	array1.typeSize = sizeof(int);
-	array1.length = 5;
-	result = findFirst(array1,isEven,&hint);
+	ArrayUtil array = { a,sizeof(int),5};
+	result = findFirst(array,isEven,&hint);
 	assertEqual(*result,6);
+};
+
+
+void test_for_findLast_return_4(){
+	int a[]={1,5,6,4,9},hint=3,*result;
+
+	ArrayUtil array = { a,sizeof(int),5};
+	result = findLast(array,isEven,&hint);
+	assertEqual(*result,4);
+};
+
+void test_for_findLast_should_return_8(){
+	int a[]={1,8,5,3,9},hint=3,*result;
+
+	ArrayUtil array = { a,sizeof(int),5};
+	result = findLast(array,isEven,&hint);
+	assertEqual(*result,8);
+};
+
+int compare1(void *hint,void* item){
+	if(*(char*)item=='a')
+		return 1;
+	return 0;
+};
+
+void test_findfirst_will_return_a(){
+	char a[]={'b','a','b','b'},hint=3,*result;
+	ArrayUtil array = {a, sizeof(char), 3};
+	result = findFirst(array,compare1,&hint);
+
+	assertEqual((char)*result,'a');
+};
+
+int compare(void* hint, void* item){
+	if(*(char *)item=='g')
+		return 1;
+	return 0;
+};
+
+void test_for_findFirst_return_g(){
+	char a[]={'a','v','g','h','g'},hint=3,*result;
+
+	ArrayUtil array = { a,sizeof(char),5};
+	result = findFirst(array,compare,&hint);
+	assertEqual((char)*result,'g');
+};
+
+void test_for_count_return_3_num_of_g(){
+	char a[]={'a','g','g','h','g'},hint=3;
+	int result;
+
+	ArrayUtil array = { a,sizeof(char),5};
+	result = count(array,compare,&hint);
+	assertEqual(result,3);
+};
+
+
+void test_for_count_return_2_num_of_4(){
+	int a[]={1,4,3,4,9},hint=3;
+	int result;
+
+	ArrayUtil array = { a,sizeof(int),5};
+	result = count(array,isEven,&hint);
+	assertEqual(result,2);
+};
+
+//-----------------------------------------------------------------------------------------------------------------
+
+void test_for_filter_that_return_the_num_of_ele_that_match_the_criteria(){
+	int a[]={1,4,3,4,4},hint=3;
+	void *destination; 
+	int resultCount;
+	ArrayUtil array = { a,sizeof(int),5};
+	resultCount = filter(array,isEven,&hint,&destination,2);
+	assertEqual(resultCount,2);
+	// assertEqual(((int *)destination)[0],4);
+};
+
+// ----------------------------------------------------------------------------------------------------------------
+
+int isEqual(void* hint, void* item){
+	if(*(int*)item==8 ||*(float*)item == 9.0 || *(double*)item==8.9)
+		return 1;
+	return 0;
 }
+
+void test_filter_will_return_the_array_of_8_8_8(){
+	int a[]={1,8,8,7,8,9},hint=3,length;
+	void* result;
+	ArrayUtil array = {a, sizeof(int), 6};
+	length = filter(array,isEqual,&hint,&result,5);
+
+	assertEqual(((int*)result)[0],8);
+	assertEqual(length,3);
+};
+
+
+void test_filter_will_return_the_array_of_only_two_8(){
+	int a[]={1,8,8,7,8,9},hint=3,length;
+	void* result;
+	ArrayUtil array = {a, sizeof(int), 6};
+	length = filter(array,isEqual,&hint,&result,2);
+
+	assertEqual(((int*)result)[1],8);
+	assertEqual(length,2);
+};
+
+void test_filter_will_return_the_array_of_only_two_8_point_7_in_float(){
+	float a[]={1.4,8.4,8.9,7,8,9.0},hint=3;
+	int length;
+	void* result;
+	ArrayUtil array = {a, sizeof(float), 6};
+	length = filter(array,isEqual,&hint,&result,2);
+
+	assertEqual(((float*)result)[0],9.0);
+	assertEqual(length,1);
+};
+
+void test_filter_will_return_the_array_a_a_a(){
+	char a[]={'g','g','g','b','d'},hint=3;
+	int length;
+	void* result;
+	ArrayUtil array = {a, sizeof(char), 5};
+	length = filter(array,compare,&hint,&result,5);
+
+	assertEqual(((char*)result)[1],'g');
+	assertEqual(length,3);
+};
